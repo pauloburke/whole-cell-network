c50be9f3d295848a59d683e35646193e52ae9be5
Paulo Burke <pauloepburke@gmail.com>
2017-01-25 01:59:05 -0200

    Add data files and old network building scripts. Also a sketch of the script of conservation correlation analysis.

---
+++ scripts/network-analysis/conservation-correlation/conservation_correlation_data_parser.py
@@ -0,0 +1,6 @@
+
+#ler arquivo mgenitalium.db
+#extrair sequencias
+#rodar blastp localmente utilizando biopyhton databases pdbaa e swissprot
+#acessar rede por igraph
+#gerar arquivo com maches no blast e grau da proteína gerada pelo gene
